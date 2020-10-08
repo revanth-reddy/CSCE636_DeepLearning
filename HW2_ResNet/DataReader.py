605d2d2fada023f16091e826be2005626ad39603
Rizu Jain <rrizujain@gmail.com>
2020-10-07 19:23:49 -0500

    Part and b done

--- HW2_ResNet/DataReader.py
+++ HW2_ResNet/DataReader.py
@@ -6,49 +6,84 @@ import numpy as np
 """
 
 def load_data(data_dir):
-	"""Load the CIFAR-10 dataset.
+    """Load the CIFAR-10 dataset.
 
-	Args:
-		data_dir: A string. The directory where data batches
-			are stored.
+    Args:
+        data_dir: A string. The directory where data batches
+            are stored.
 
-	Returns:
-		x_train: An numpy array of shape [50000, 3072].
-			(dtype=np.float32)
-		y_train: An numpy array of shape [50000,].
-			(dtype=np.int32)
-		x_test: An numpy array of shape [10000, 3072].
-			(dtype=np.float32)
-		y_test: An numpy array of shape [10000,].
-			(dtype=np.int32)
-	"""
+    Returns:
+        x_train: An numpy array of shape [50000, 3072].
+            (dtype = np.float32)
+        y_train: An numpy array of shape [50000, ].
+            (dtype = np.int32)
+        x_test: An numpy array of shape [10000, 3072].
+            (dtype = np.float32)
+        y_test: An numpy array of shape [10000, ].
+            (dtype = np.int32)
+    """
 
-	### YOUR CODE HERE
+    ### YOUR CODE HERE
+    num_train_batches = 5
+    img_pixels = 32
 
-	
-	### END CODE HERE
+    def unpickle(file):
+        with open(file, 'rb') as fo:
+            dict = pickle.load(fo, encoding = 'bytes')
+        return dict
 
-	return x_train, y_train, x_test, y_test
+    def get_data(_filepath):
+        data = unpickle(_filepath)
 
-def train_valid_split(x_train, y_train, split_index=45000):
-	"""Split the original training data into a new training dataset
-	and a validation dataset.
+        raw_img_float = np.array(data[b'data'], dtype = float) / 255.0
+        img = raw_img_float.reshape([-1, 3, img_pixels, img_pixels])
+        img = img.transpose([0, 2, 3, 1])
+        lbl = np.array(data[b'labels'])
+        return img, lbl
 
-	Args:
-		x_train: An array of shape [50000, 3072].
-		y_train: An array of shape [50000,].
-		split_index: An integer.
+    images = np.zeros(shape = [0, img_pixels, img_pixels, 3], dtype = float)
+    labels = np.asarray([], dtype = int)
 
-	Returns:
-		x_train_new: An array of shape [split_index, 3072].
-		y_train_new: An array of shape [split_index,].
-		x_valid: An array of shape [50000-split_index, 3072].
-		y_valid: An array of shape [50000-split_index,].
-	"""
-	x_train_new = x_train[:split_index]
-	y_train_new = y_train[:split_index]
-	x_valid = x_train[split_index:]
-	y_valid = y_train[split_index:]
+    # For each data-file.
+    for i in range(num_train_batches):
+        filename = "data_batch_" + str(i + 1)
+        filepath = os.path.join(data_dir, filename)
+        images_batch, labels_batch = get_data(filepath)
+        images = np.concatenate((images, images_batch))
+        labels = np.concatenate((labels, labels_batch))
 
-	return x_train_new, y_train_new, x_valid, y_valid
+    x_train, y_train = images, labels
+
+    filename = "test_batch"
+    filepath = os.path.join(data_dir, filename)
+    x_test, y_test = get_data(filepath)
+
+    ### END CODE HERE
+    print(x_train.shape)
+    print(y_train.shape)
+    print(x_test.shape)
+    print(y_test.shape)
+    return x_train, y_train, x_test, y_test
+
+def train_valid_split(x_train, y_train, split_index = 45000):
+    """Split the original training data into a new training dataset
+    and a validation dataset.
+
+    Args:
+        x_train: An array of shape [50000, 3072].
+        y_train: An array of shape [50000, ].
+        split_index: An integer.
+
+    Returns:
+        x_train_new: An array of shape [split_index, 3072].
+        y_train_new: An array of shape [split_index, ].
+        x_valid: An array of shape [50000-split_index, 3072].
+        y_valid: An array of shape [50000-split_index, ].
+    """
+    x_train_new = x_train[:split_index]
+    y_train_new = y_train[:split_index]
+    x_valid = x_train[split_index:]
+    y_valid = y_train[split_index:]
+
+    return x_train_new, y_train_new, x_valid, y_valid
 
--- HW2_ResNet/ImageUtils.py
+++ HW2_ResNet/ImageUtils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 """This script implements the functions for data augmentation
 and preprocessing.
 """
@@ -41,30 +41,28 @@ def preprocess_image(image, training):
 		### YOUR CODE HERE
 		# Resize the image to add four extra pixels on each side.
 		# image = tf.image.resize_image_with_crop_or_pad(image, 32 + 8, 32 + 8)
-		
-
+		image = np.pad(image,((4,4),(4,4),(0,0)),'constant')
 		### END CODE HERE
-
 		### YOUR CODE HERE
 		# Randomly crop a [32, 32] section of the image.
 		# image = tf.random_crop(image, [32, 32, 3])
 		# HINT: randomly generate the upper left point of the image
-		
-
+		x_offset = random.randrange(1,9)
+		y_offset = random.randrange(1,9)
+		image = image[x_offset:x_offset+32, y_offset:y_offset+32,:]
 		### END CODE HERE
 
 		### YOUR CODE HERE
 		# Randomly flip the image horizontally.
 		# image = tf.image.random_flip_left_right(image)
-		
-
+		if random.random() < 0.5:
+			image = np.flip(image,1)
 		### END CODE HERE
 
 	### YOUR CODE HERE
 	# Subtract off the mean and divide by the standard deviation of the pixels.
 	# image = tf.image.per_image_standardization(image)
-	
-	
+	image = (image-np.mean(image))/np.std(image);
 	### END CODE HERE
 
 	return image
